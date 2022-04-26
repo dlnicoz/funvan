8c235aee045ed292395a825fd0a1cf550f0ba8a1
iori <92986739+dlnicoz@users.noreply.github.com>
2022-04-27 00:49:59 +0530

    a funvan

---
+++ min-max.py
@@ -0,0 +1,8 @@
+def maxminposition(A,n):
+    print ("The maximum is at position::", maxposition + 1)
+    print ("The minimum is at position::", minposition + 1)
+    n=int(input("Enterthe size ofthe List::"))
+    print("Enter the Element ::")
+for i in range(int(n)):k=int(input(""))
+    A.append(k)
+    maxminposition(A,n)
